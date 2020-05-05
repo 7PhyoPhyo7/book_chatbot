0ebff3f66ee50e356564074c1ca5a6ac923f04b5
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-05-06 03:00:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -186,7 +186,7 @@ app.post('/webhook', (req, res) => {
                                       "persistent_menu":[
                                       {
                                         "locale":"default",
-                                        "composer_input_disabled":true,
+                                        "composer_input_disabled":false,
                                         "call_to_actions":[
                                         {
                                             "type":"postback",

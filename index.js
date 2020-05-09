5a73bc92444401fdffa6ca53335e4fa6f34b8568
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-05-09 15:35:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -238,7 +238,9 @@ app.post('/webhook', (req, res) => {
                             {
                               console.log("UserMessagevideo",userMessage);
                                //console.log("UM",userMessage);
-                                 VideoUpload(senderID,userMessage)
+                                 VideoUpload(senderID,userMessage).then(success=>{
+                                  textMessage(senderID,"Upload Successful");
+                                 })
                                  uploadvideo='';
                                  
                             }

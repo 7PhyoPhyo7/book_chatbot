3c5edbb1b371630d6357282d18756206a71c11e1
DESKTOP-DELTEBT\KHIN YADANA PHYO <khinyadanaphyo@u.nus.edu>
2020-05-11 16:06:50 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1164,11 +1164,9 @@ app.post('/edit_book', (req, res) => {
     ownerid: sender
   }).then(success => {
     textMessage(sender, "Update Successful").then(io=>{
-      QuickReplyMenu(senderID);
-    }).then(uuuu=>{
-      res.status(200).send("Update Successful and Please go back to your messages and please check your book detail");
+      QuickReplyMenu(sender);
     })
-    
+    res.status(200).send("Update Successful and Please go back to your messages and please check your book detail");
     // window.location.assign('https://www.messenger.com/closeWindow/?image_url=https://secure.i.telegraph.co.uk/multimedia/archive/03058/thankyou-interest_3058089c.jpg&display_text=Thanks');
   }).catch(error => {
     console.log(error);
